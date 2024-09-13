e952107099d907e0926fa807f006e81c11028459
Ricardo de Genova <ricgnv@gmail.com>
2024-09-13 13:07:02 -0400

    completed first version of Models

--- AnnPrepareLavni.ApiService/Models/Patient.cs
+++ AnnPrepareLavni.ApiService/Models/Patient.cs
@@ -11,20 +11,21 @@ public class Patient
         CreatedAt = DateTime.Now;
     }
 
-    public required Guid Id { get; init; }
+    public Guid Id { get; init; }
     public int PatientNo { get; init; }
-    public required string FirstName { get; set; } = string.Empty;
-    public required string LastName { get; set; } = string.Empty;
-    public required DateTime DateOfBirth { get; set; }
-    public required Gender Gender { get; set; }
-    public required float HeightInMeters { get; set; }
-    public required float WeightInKilograms { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime DateOfBirth { get; set; }
+    public Gender Gender { get; set; }
+    public float HeightInMeters { get; set; }
+    public float WeightInKilograms { get; set; }
     public int FamilySize { get; set; }
     public HighestEducation HighestEducation { get; set; }
     public List<MedicalCondition> MedicalConditions { get; set; }
     public string SurgicalProceduresNotes { get; set; }
     public string FamilyHistoryNotes { get; set; }
     public string AllergiesNotes { get; set; }
+    public string ProfileNotes { get; set; }
 
     public Guid AddressId { get; set; }
     public Address Address { get; set; }
