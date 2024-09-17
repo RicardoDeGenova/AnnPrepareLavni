f3e6f46415af95c38c9d4aad7c578e6e4f63bd3d
Ricardo de Genova <ricgnv@gmail.com>
2024-09-17 17:00:12 -0400

    organize program.cs

--- AnnPrepareLavni.ApiService/Program.cs
+++ AnnPrepareLavni.ApiService/Program.cs
@@ -1,16 +1,14 @@
 using AnnPrepareLavni.ApiService.Data;
-using AnnPrepareLavni.ApiService.Features.Patient;
-using AnnPrepareLavni.ApiService.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("AnnPrepareLavniDb"));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
@@ -21,14 +19,10 @@ builder.Services.AddCors(options =>
                .AllowAnyHeader());
 });
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseInMemoryDatabase("AnnPrepareLavniDb"));
-
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
